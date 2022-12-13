619577ff2d2c101eb4b321de3315f5bd9d26cef5
evanszs <rafael_mendes_1999@hotmail.com>
2022-12-13 02:16:11 -0300

    1

---
+++ src/pages/Header.js
@@ -0,0 +1,20 @@
+import '../css/header.css'
+import fundo from '../img/truelove.jpg'
+const Header = () => {
+  return (
+  
+
+
+    <div >
+      <div>
+      <h1 class="page-title">True love </h1>
+    <header class="header bd"> 
+
+     </header>
+
+
+     </div></div>
+  )
+}
+
+export default Header
---
+++ src/pages/Meio.js
@@ -0,0 +1,209 @@
+import barra from '../img/barra.jpg'
+import ambiente from '../img/ambiente2.jpg'
+import pessoas from '../img/ppl.jpg'
+import Accordion from 'react-bootstrap/Accordion';
+import fundo from '../img/fundo.png'
+import Footer from './footer';
+import wpp from '../img/whatsapp.png'
+import ig from '../img/instagram.png'
+const Meio = () => {
+  return (
+    <div>
+<main class="main">
+      <section class="section">
+        <figure class="image-container"> <img src={ambiente} style={{opacity:'0.3'}}/> </figure>
+ 
+        <h4></h4>
+        <article class="content">
+          <h2 class="section-title" style={{textAlign:"center"}}>Horário/Local</h2>
+          <p style={{textAlign:"center",textJustify:'inter-word'}}>
+          
+<h1 > Nossa localização</h1> 
+
+<p>
+O estudio fica na Av. Oceanica, 551, no Ed, Barra Center, sobreloja 8.
+</p>
+<p>
+<h1 style={{marginTop:"50px"}}>Funcionamento</h1>
+<ul> Todos os dias porém com hora marcada começando as 11:00 e terminando as 19:00</ul>
+<ul> Os atendimentos são feitos com hora marcada 
+    e os agendamentos podem ser feitos diretamente com os tatuadores via Instagram ou
+     Whatsapp.</ul>
+<ul> Não esqueca de seguir o estúdio nas redes sociais (@tl.inke) para acompanhar 
+     nossos projetos e eventos, como flashdays, comemorações e novidades.</ul>
+
+</p> <div style={{paddingBottom:"250px"}}></div>
+
+
+          </p>
+        </article>
+      </section>
+      <section class="section">
+        <figure class="image-container"><img src={barra} alt="" style={{opacity:'0.3'}}/></figure>
+        <article class="content"  style={{textAlign:"center",textJustify:'inter-word'}}>
+          <h2 class="section-title">O que eu devo saber?</h2>
+          
+          
+          <span class="byline">Um pequeno FAQ para os curiosos de plantão </span>
+          <Accordion>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Qual o valor mínimo do estúdio?</Accordion.Header>
+        <Accordion.Body>
+    <p>   O valor mínimo cobrado pelas tatuagens no estúdio é de R$250 e pode variar
+dependendo do tamanho, local e detalhes. O orçamento pode ser feito diretamente
+com os nossos artistas.</p> 
+
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+
+        <Accordion.Header>Posso fazer uma tatuagem sem marcar horário?</Accordion.Header>
+        <Accordion.Body>
+      <p>  Sim, dependendo da disponibilidade dos artistas no dia nos aceitamos fazer trabalhos
+’’não programados’’. Mas sugerimos você checar a disponibilidade antes de ir ao
+estúdio via mensagem por social media.</p> 
+
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+
+<Accordion.Header>Como eu posso agendar um horário?</Accordion.Header>
+<Accordion.Body>
+<p>A melhor forma de agendar uma tattoo e diretamente com os nossos artistas, via social
+media ou whatsapp, por lá nos conseguimos ter um contato mais proximos e captar
+todas as ideias que você teve para a criação da arte.
+Quando for solicitar um orcamento, tente nos dar o mais numero de informacoes
+possiveis sobre a tattoo que você planeja ter, como:
+Local, tamanho em cm, a sua ideia e detalhes.
+Fique a vontade para mandar referência , elas nos ajudam a entender melhor como
+você idealizou a sua tattoo.
+Para que o seu horário seja resevado e necessário o pagamento adiantado de um sinal
+no valor de R$50,00. Este sinal será abatido no valor final da sessão.
+Ex: se o valor da tatuagem e 350 reais o sinal depositado e de 50 reais e no final da
+sessão você paga 300 reais) </p>
+</Accordion.Body>
+</Accordion.Item>
+<Accordion.Item eventKey="3">
+
+<Accordion.Header>Quanto vai custar a minha tatuagem?
+</Accordion.Header>
+<Accordion.Body>
+<p> Nós sabemos como é difícil juntar dinheiro para uma tatto, então queremos que você
+tenha uma noção de quanto cobramos nas nossas tatuagens
+Os preços médios no estúdio são :
+Pequena (palavras ou desenhos ate 5 cm) - R$250
+Médiia(frase ou desenhos ate 8 cm)- R$300~400
+Grande (desenhos ate 15cm)- R$500~700
+Fechamentos (braço, perna, costas)-R$1500~3000
+ Lembrando que os valores estão sujeitos a alteração dependendo do artista que fizer
+e a quantidade de detalhes da arte final.</p>
+</Accordion.Body>
+</Accordion.Item>
+<Accordion.Item eventKey="4">
+
+<Accordion.Header>Posso consultar o artista antes da sessao?
+</Accordion.Header>
+<Accordion.Body>
+<p> Sim, nossos artistas sao capacitados para fazer coberturas de todos os tipos. MAs nem
+sempre é possivel fazer exatamente o que voce esta pensando pois geralmente
+coberturas tem mais limitações, diferentes de projetos criados do zero. Por isso seja
+flexível quanto considerar a ideia sugerida pelo profissional, já que ele busca fazer o
+melhor trabalho</p>
+</Accordion.Body>
+</Accordion.Item>
+<Accordion.Item eventKey="5">
+
+<Accordion.Header>E a minha primeira tattoo, o que devo saber?</Accordion.Header>
+<Accordion.Body>
+<p>Todos os dias encontramos pessoas interessadas em fazer sua primeira tatuagem
+conosco. Então sim, nós sabemos como você se sente..
+Textos Site 4
+Não há limite de idade para a primeira tatuagem, exceto para menores de idade.
+Algumas pessoas fazem sua primeira tatuagem no final dos anos 80, então você não
+deve ter vergonha de dizer isso ao seu artista.
+Nós pedimos que você nos avise se for sua primeira vez, então sinta-se à vontade para
+perguntar ao seu artista tudo o que você precisa saber antes de começar.
+Quando finalmente chegar o dia da sua consulta, siga estas regras simples:
+Não beba álcool no caminho e tente ficar sóbrio desde o dia anterior. O álcool é um
+anticoagulante e tem o potencial de complicar as coisas enquanto você está sendo
+tatuado.
+Faça uma refeição antes da consulta.
+Mantenha os níveis de açúcar elevados, mantenha-se hidratado e tenha alguns
+lanches à mão.
+Diga ao artista se você está se sentindo tonto ou fraco, isso é completamente
+normal, mas precisamos saber para administrar a situação com cuidado.
+A sessão pode demorar mais do que você pensou inicialmente, então é melhor
+manter sua agenda o mais flexível possível.
+Dependendo da localização da sua tatuagem, recomendamos que você use roupas
+adequadas, a fim de facilitar tanto para você quanto para o tatuador. Também
+desaconselhamos o uso de roupas caras ou roupas brancas, pois elas podem
+manchar facilmente.
+A higiene pessoal é importante! Por favor, certifique-se de estar limpo, em respeito
+ao tatuador e às outras pessoas na loja.
+Depois de ser tatuado por nós, por favor, fique longe; sol, banhos e praia. A tatuagem
+precisa ser cuidada por um mês depois de concluída, e não ficará igual se você fizer o
+contrário! Se você não seguir nossas diretrizes, poderá correr o risco de possíveis
+infecções.
+Por fim, se você estiver prestes a viajar para as férias de verão, talvez seja melhor
+remarcar a sessão.
+</p>
+</Accordion.Body>
+</Accordion.Item>
+<Accordion.Item eventKey="6">
+
+<Accordion.Header>Posso levar acompanhante?</Accordion.Header>
+<Accordion.Body>
+<p>É possivel levar acompanhante, mas saiba que muitas vezes a espera na sessao e
+longa, e nem sempre e possivel acompanhar a tatto de perto, entao todos devem vir
+preparados.</p>
+</Accordion.Body>
+</Accordion.Item>
+    </Accordion>
+    <img src={fundo}></img>
+        </article>
+      </section>
+      <section class="section">
+
+       <figure class="image-container">       <img src={pessoas} style={{opacity:'0.3'}}/></figure>
+        <article class="content">
+          <h2 class="section-title">Um pouco sobre nós</h2><span class="byline">True love </span>
+    
+<h1> De onde surgimos</h1>
+<p>É um estúdio de tatuagem idealizado e criado em 2020 pelos artistas e tatuadores Erick e Drica Torrez. Localizado no na Barra, o coração de Salvador Bahia e 
+de frente para o mar, o estúdio é a casa de quatro tatuadores talentosos e guests renomados. 
+Arte autoral com qualidade e a melhor experiencia em atendimento é o que mais prezamos.
+</p>
+<p>
+ O estúdio foi projetado para você se sentir em casa e desfrutar de uma vista
+  incrível enquanto tatua com os melhores profissionais. Erick Torrez <a><img src={ig} width="5%"/></a>
+   Drica Torrez <a><img src={ig} width="5%"/></a>, Gagabi <a><img src={ig} width="5%"/></a>e Adailton<a><img src={ig} width="5%"/></a>
+ são os nossos tatuadores, e cada um tem o seu estilo próprio e
+    autoral.
+    </p>
+        </article>
+      </section>
+
+    </main>
+ 
+<footer className='footer'>
+    <form>
+<input type="email" placeholder='Enviar um E-mail' required></input> <button>Teste</button></form>
+<span style={{textAlign:'center'}}><p>© 2022 All Rights Reserved | Truelove Tattoo Art  </p> 
+<a><img src={ig} width="5%" style={{marginRight:'10px'}}/></a>
+<a><img src={wpp} width="5%"/></a>
+ 
+
+</span>
+
+ 
+
+<img src={fundo}></img>
+
+
+</footer>
+
+ </div>
+  )
+}
+
+export default Meio
